d6fcc71f8ebb51cbc1da3d7c7a77c6f5b7aa03bc
NiNjA <imniranjanbabu@gmail.com>
2019-07-09 13:08:45 +0530

    different functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-function App() {
-  return (
-    <p>Hi Hello</p>
-  );
-}
+// function App() {
+//   return (
+//     <p>Hi Hello</p>
+//   );
+// }
+
+// const App = () => {
+//   return(
+//     <p>Hi Hello</p>
+//   );
+// }
+
+// const App = () => (<p>Hi Hello</p>);
+
+const App = () => <p>Hi Hello</p>;
+
+
 
 export default App;
